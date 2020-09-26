ba63d66d058fc83d7289ff46e30ca1b6ebdde534
Phil Ondrejack <pondrejack@chain.io>
2020-09-26 10:21:08 -0700

    continuous 12

--- src/forms/EditListingAvailabilityForm/ProductAvailabilityCalendar.js
+++ src/forms/EditListingAvailabilityForm/ProductAvailabilityCalendar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react';
 import { DayPilot, DayPilotScheduler } from 'daypilot-pro-react'
 import { connect } from 'react-redux'
 import {
@@ -6,169 +6,117 @@ import {
   deleteEventFromAvailabilityCalendar, fetchAvailabilityCalendar, updateEventOnAvailabilityCalendar,
 } from '../../containers/EditListingPage/EditListingPage.duck'
 
-class Scheduler extends Component {
-  constructor(props) {
-    super(props)
+const Scheduler = (props) => {
+  let scheduler
 
-    this.state = {
-      timeHeaders: [{ groupBy: 'Month' }, { groupBy: 'Day', format: 'd' }],
-      scale: 'Day',
-      days: DayPilot.Date.today().daysInYear(),
-      startDate: DayPilot.Date.today().firstDayOfYear(),
-      timeRangeSelectedHandling: 'Enabled',
-      onTimeRangeSelected(args) {
-        const dp = this
-        DayPilot.Modal.prompt('Create a new event:', 'Event 1').then((modal) => {
-          dp.clearSelection()
-          if (!modal.result) { return }
-          dp.events.add(new DayPilot.Event({
-            start: args.start,
-            end: args.end,
-            id: DayPilot.guid(),
-            resource: args.resource,
-            text: modal.result
-          }))
-        })
-      },
-      eventMoveHandling: 'Update',
-      onEventMoved(args) {
-        this.message(`Event moved: ${args.e.text()}`)
-      },
-      eventResizeHandling: 'Update',
-      onEventResized(args) {
-        this.message(`Event resized: ${args.e.text()}`)
-      },
-      eventDeleteHandling: 'Update',
-      onEventDeleted(args) {
-        this.message(`Event deleted: ${args.e.text()}`)
-      },
-      eventClickHandling: 'Disabled',
-      eventHoverHandling: 'Bubble',
-      bubble: new DayPilot.Bubble({
-        onLoad(args) {
-          // if event object doesn't specify "bubbleHtml" property
-          // this onLoad handler will be called to provide the bubble HTML
-          args.html = 'Event details'
-        }
-      }),
-      treeEnabled: true,
-    }
+  useEffect(() => {
+
+  }, [])
+
+  const config = {
+    timeHeaders: [{ groupBy: 'Month' }, { groupBy: 'Day', format: 'd' }],
+    scale: 'Day',
+    days: DayPilot.Date.today().daysInYear(),
+    startDate: DayPilot.Date.today(),
+    timeRangeSelectedHandling: 'Enabled',
+    onTimeRangeSelected(args) {
+      const dp = scheduler
+      DayPilot.Modal.prompt('Create a new event:', 'Event 1').then((modal) => {
+        dp.clearSelection()
+        if (!modal.result) { return }
+        dp.events.add(new DayPilot.Event({
+          start: args.start,
+          end: args.end,
+          id: DayPilot.guid(),
+          resource: args.resource,
+          text: modal.result
+        }))
+      })
+    },
+    eventMoveHandling: 'Update',
+    onEventMoved(args) {
+      scheduler.message(`Event moved: ${args.e.text()}`)
+    },
+    eventResizeHandling: 'Update',
+    onEventResized(args) {
+      scheduler.message(`Event resized: ${args.e.text()}`)
+    },
+    eventDeleteHandling: 'Update',
+    onEventDeleted(args) {
+      scheduler.message(`Event deleted: ${args.e.text()}`)
+    },
+    eventClickHandling: 'Disabled',
+    eventHoverHandling: 'Bubble',
+    bubble: new DayPilot.Bubble({
+      onLoad(args) {
+        // if event object doesn't specify "bubbleHtml" property
+        // this onLoad handler will be called to provide the bubble HTML
+        args.html = 'Event details'
+      }
+    }),
+    treeEnabled: true,
   }
 
-  componentDidMount() {
-    // load resource and event data
-    this.setState({
-      resources: [
-        { name: 'Resource A', id: 'A' },
-        { name: 'Resource B', id: 'B' },
-        { name: 'Resource C', id: 'C' },
-        { name: 'Resource D', id: 'D' },
-        { name: 'Resource E', id: 'E' },
-        { name: 'Resource F', id: 'F' },
-        { name: 'Resource G', id: 'G' }
-      ],
-      events: [
-        {
-          id: 1,
-          text: 'Event 1',
-          start: '2018-05-02T00:00:00',
-          end: '2018-05-05T00:00:00',
-          resource: 'A'
-        },
-        {
-          id: 2,
-          text: 'Event 2',
-          start: '2018-05-03T00:00:00',
-          end: '2018-05-10T00:00:00',
-          resource: 'C',
-          barColor: '#38761d',
-          barBackColor: '#93c47d'
-        },
-        {
-          id: 3,
-          text: 'Event 3',
-          start: '2018-05-02T00:00:00',
-          end: '2018-05-08T00:00:00',
-          resource: 'D',
-          barColor: '#f1c232',
-          barBackColor: '#f1c232'
-        },
-        {
-          id: 4,
-          text: 'Event 3',
-          start: '2018-05-02T00:00:00',
-          end: '2018-05-08T00:00:00',
-          resource: 'E',
-          barColor: '#cc0000',
-          barBackColor: '#ea9999'
-        }
-      ]
-    })
-  }
-
-  render() {
-    const { ...config } = this.state
-    return (
-      <div>
-        <DayPilotScheduler
-          {...config}
-          resources={[
-            {
-              name: 'Group 1',
-              id: 'G1',
-              expanded: true,
-              children: [
-                {
-                  name: 'Resource 1',
-                  id: 'R1'
-                },
-                {
-                  name: 'Resource 2',
-                  id: 'R2'
-                }
-              ]
-            },
-            {
-              name: 'Group 2',
-              id: 'G2',
-              expanded: true,
-              children: [
-                {
-                  name: 'Resource 3',
-                  id: 'R3'
-                },
-                {
-                  name: 'Resource 4',
-                  id: 'R4'
-                }
-              ]
-            }
-          ]}
-          events={
-            [
+  return (
+    <div>
+      <DayPilotScheduler
+        {...config}
+        resources={[
+          {
+            name: 'Group 1',
+            id: 'G1',
+            expanded: true,
+            children: [
               {
-                id: 1,
-                resource: 'R1',
-                start: '2020-09-04T00:00:00',
-                end: '2020-09-08T00:00:00',
-                text: 'Event 1'
+                name: 'Resource 1',
+                id: 'R1'
               },
               {
-                id: 2,
-                resource: 'R1',
-                start: '2020-09-12T00:00:00',
-                end: '2020-09-16T00:00:00',
-                text: 'Event 2'
+                name: 'Resource 2',
+                id: 'R2'
+              }
+            ]
+          },
+          {
+            name: 'Group 2',
+            id: 'G2',
+            expanded: true,
+            children: [
+              {
+                name: 'Resource 3',
+                id: 'R3'
+              },
+              {
+                name: 'Resource 4',
+                id: 'R4'
               }
             ]
           }
-          ref={(component) => {
-            this.scheduler = component && component.control
-          }}
-        />
-      </div>
-    )
-  }
+        ]}
+        events={
+          [
+            {
+              id: 1,
+              resource: 'R1',
+              start: '2020-09-04T00:00:00',
+              end: '2020-09-08T00:00:00',
+              text: 'Event 1'
+            },
+            {
+              id: 2,
+              resource: 'R1',
+              start: '2020-09-12T00:00:00',
+              end: '2020-09-16T00:00:00',
+              text: 'Event 2'
+            }
+          ]
+        }
+        ref={(component) => {
+          scheduler = component && component.control
+        }}
+      />
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
